903c49b259f0651e7b8367a32d5b76f9cc879188
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-25 05:07:27 -0500

    connecting react to firebase

--- firebase_react/applications/react-firebase-first-flight-master/src/App.js
+++ firebase_react/applications/react-firebase-first-flight-master/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
+import { database } from './firebase';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App--header">
-          <h2>Welcome to React and Firebase</h2>
-        </div>
-        <pre className="App--data">
-          One day, some data from Firebase will go here.
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: null
+		};
+	}
+
+	componentDidMount() {
+		database.ref('/').on('value', () => {
+			console.log('THE DATA CHANGED!');
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<div className="App--header">
+					<h2>Welcome to React and Firebase</h2>
+				</div>
+				<pre className="App--data">
+					One day, some data from Firebase will go here.
         </pre>
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default App;
