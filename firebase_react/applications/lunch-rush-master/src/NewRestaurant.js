a3e27bc89bfa3b8a6a697370d55960bce9083565
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-25 23:13:01 -0500

    content for logged in users

--- firebase_react/applications/lunch-rush-master/src/NewRestaurant.js
+++ firebase_react/applications/lunch-rush-master/src/NewRestaurant.js
@@ -1,46 +1,49 @@
 import React, { Component, PropTypes } from 'react';
+import { database } from './firebase';
 import './NewRestaurant.css';
 
 class NewRestaurant extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: ''
-    };
+	constructor() {
+		super();
+		this.state = {
+			name: ''
+		};
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+		this.restaurantsRef = database.ref('/restaurants');
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-  handleSubmit(event) {
-    event.preventDefault();
-  }
+	handleSubmit(event) {
+		event.preventDefault();
+		this.restaurantsRef.push({ name: this.state.name });
+	}
 
-  render() {
-    const { name } = this.state;
+	render() {
+		const { name } = this.state;
 
-    return (
-      <form
-        className="NewRestaurant"
-      >
-        <input
-          type="text"
-          value={ name }
-          placeholder="Name of Fine Establishment"
-          onChange={(event) => this.setState({ name: event.target.value })}
-        />
-        <button
-          onClick={this.handleSubmit}
-          disabled={!name}
-        >
-          Submit
+		return (
+			<form
+				className="NewRestaurant"
+			>
+				<input
+					type="text"
+					value={name}
+					placeholder="Name of Fine Establishment"
+					onChange={(event) => this.setState({ name: event.target.value })}
+				/>
+				<button
+					onClick={this.handleSubmit}
+					disabled={!name}
+				>
+					Submit
         </button>
-      </form>
-    );
-  }
+			</form>
+		);
+	}
 }
 
 NewRestaurant.propTypes = {
-  restaurantsRef: PropTypes.object
+	restaurantsRef: PropTypes.object
 };
 
 export default NewRestaurant;
