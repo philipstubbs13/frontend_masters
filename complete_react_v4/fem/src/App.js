70ee4583f63c3a79081a154e667a63a3a3d5ee76
philipstubbs13 <philipstubbs13@gmail.com>
2018-10-04 23:50:19 -0500

    pure react

---
+++ complete_react_v4/fem/src/App.js
@@ -0,0 +1,35 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+}
+
+class App extends React.Component {
+  handleTitleClick() {
+    alert("you clicked the title");
+  }
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement('h1', { onClick: this.handleTitleClick }, 'Adopt Me!'),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        name: "Doink",
+        animal: "cat",
+        breed: "Mixed"
+      })
+    ])
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
