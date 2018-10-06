4f18649f359402441cade4181185184e68bd3e5f
philipstubbs13 <philipstubbs13@gmail.com>
2018-10-06 05:45:06 -0500

    parcel

--- complete_react_v4/fem/src/App.js
+++ complete_react_v4/fem/src/App.js
@@ -1,12 +1,7 @@
 /* global React ReactDOM */
-
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 class App extends React.Component {
   handleTitleClick() {
@@ -38,4 +33,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
