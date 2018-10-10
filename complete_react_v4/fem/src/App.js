afd3ee553dbdb766b5f3821cc69b9d8e23bb3df0
philipstubbs13 <philipstubbs13@gmail.com>
2018-10-10 05:42:08 -0500

    component routing

--- complete_react_v4/fem/src/App.js
+++ complete_react_v4/fem/src/App.js
@@ -1,72 +1,19 @@
 /* global React ReactDOM */
 import React from "react";
 import { render } from "react-dom";
-import pf from "petfinder-client";
-import Pet from "./Pet";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import { Router } from "@reach/router";
+import Results from "./Results";
+import Details from "./Details";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pets: []
-    };
-  }
-  componentDidMount() {
-    petfinder.pet
-      .find({ output: "full", location: "Seattle, WA" })
-      .then(data => {
-        let pets;
-
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet;
-          } else {
-            pets = [data.petfinder.pets.pet];
-          }
-        } else {
-          pets = [];
-        }
-
-        this.setState({
-          pets
-        });
-      });
-  }
-
   render() {
     return (
       <div>
-        <h1>Adopt Me</h1>
-        {/* <pre>
-          <code>{JSON.stringify(this.state, null, 4)}</code>
-        </pre> */}
-        <div>
-          {this.state.pets.map(pet => {
-            let breed;
-
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(", ");
-            } else {
-              breed = pet.breeds.breed;
-            }
-            return (
-              <Pet
-                key={pet.id}
-                animal={pet.animal}
-                name={pet.name}
-                breed={breed}
-                media={pet.media}
-                location={`${pet.contact.city}, ${pet.contact.state}`}
-              />
-            );
-          })}
-        </div>
+        <h1>Adopt Me!</h1>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
---
+++ complete_react_v4/fem/src/Details.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Details extends React.Component {
+  render() {
+    return <h1>hi lol</h1>;
+  }
+}
+
+export default Details;
