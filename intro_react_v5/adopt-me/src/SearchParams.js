ca50caee40b43624ef76e7916ed293e7e1e43b56
philipstubbs13 <philipstubbs13@gmail.com>
2019-06-08 13:40:59 -0500

    hooks

--- intro_react_v5/adopt-me/src/SearchParams.js
+++ intro_react_v5/adopt-me/src/SearchParams.js
@@ -4,9 +4,19 @@ import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, updateLocation] = useState("Seattle, WA");
-  const [breeds, updateBreeds] = useState([]);
+  const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
