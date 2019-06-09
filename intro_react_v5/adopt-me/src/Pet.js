072343178fd9bcfc269dfd1efe11a5f8a32431aa
philipstubbs13 <philipstubbs13@gmail.com>
2019-06-08 23:01:37 -0500

    class components

--- intro_react_v5/adopt-me/src/Pet.js
+++ intro_react_v5/adopt-me/src/Pet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 export default function Pet({ name, animal, breed, media, location, id }) {
   let hero = "http://placecorgi.com/300/300";
   if (media.length) {
@@ -6,7 +7,7 @@ export default function Pet({ name, animal, breed, media, location, id }) {
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -14,6 +15,6 @@ export default function Pet({ name, animal, breed, media, location, id }) {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 }
