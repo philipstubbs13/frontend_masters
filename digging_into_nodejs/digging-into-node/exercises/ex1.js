57456f69f9bc5747bddcd44df2072c93fee632b5
philipstubbs13 <philipstubbs13@gmail.com>
2019-07-18 22:43:27 -0500

    nodejs

--- digging_into_nodejs/digging-into-node/exercises/ex1.js
+++ digging_into_nodejs/digging-into-node/exercises/ex1.js
@@ -4,16 +4,49 @@
 //  chmod u+x ex1.js
 // ./ex1.js
 
+var util = require("util");
+var path = require('path');
+var fs = require('fs');
+var getStdin = require("get-stdin");
+
 var args = require("minimist")(process.argv.slice(2), {
-  boolean: ["help"],
+  boolean: ["help", "in"],
   string: ["file"]
 });
+
+var BASE_PATH = path.resolve(
+  process.env.BASE_PATH || __dirname
+);
+
+if (process.env.HELLO) {
+  console.log(process.env.HELLO);
+}
 
 if (args.help) {
   printHelp();
 }
+else if (
+  args.in ||
+  args._.includes("-")
+) {
+  getStdin().then(processFile).catch(error);
+}
 else if (args.file) {
-  console.log(args.file);
+  fs.readFile(path.join(BASE_PATH,args.file), function onContents(err, contents){
+    if (err) {
+      error(err.toString());
+    } else {
+      processFile(contents.toString());
+      // BASE_PATH=files/  ./ex1.js --file=hello.txt
+      // contents = contents.toString().toUpperCase();
+      // process.stdout.write(contents);
+    }
+  });
+
+  // processFile(path.resolve(args.file));
+  // let filepath = path.resolve(args.file);
+  // console.log(__dirname);
+  // console.log(filepath);
 }
 else {
   error("Incorrect usage", true);
@@ -39,7 +72,14 @@ else {
 
 // **************************
 
-function error(msg, includeHelp = false) {
+function processFile(contents) {
+  // var contents = fs.readFileSync(filepath, "utf8");
+  // process.stdout.write(contents);
+  contents = contents.toUpperCase();
+  process.stdout.write(contents.toString());
+}
+
+function  error(msg, includeHelp = false) {
   console.error(msg);
   if (includeHelp) {
     console.log("");
@@ -53,5 +93,9 @@ function printHelp() {
   console.log("");
   console.log("--help               print this help");
   console.log("--file={FILENAME}    process the file");
+  console.log("--in, -              process stdin");
   console.log("");
 }
+
+// Environemt variables
+// HELLO=WORLD ./ex1.js
