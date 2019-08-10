179c9c7592e7f2219e308a63432530d6f992a4c8
philipstubbs13 <philipstubbs13@gmail.com>
2019-08-10 14:42:41 -0500

    service workers

--- exploring_service_workers/service-workers/service-worker-exercise/web/js/blog.js
+++ exploring_service_workers/service-workers/service-worker-exercise/web/js/blog.js
@@ -25,11 +25,13 @@
     window.addEventListener("online", function online(){
       offlineIcon.classList.add("hidden");
       isOnline = true;
+      sendStatusUpdate();
     });
 
     window.addEventListener("offline", function offline(){
       offlineIcon.classList.remove("hidden");
       isOnline = false;
+      sendStatusUpdate();
     });
 	}
 
@@ -39,10 +41,38 @@
     });
 
     svcworker = swRegistration.installing || swRegistration.waiting || swRegistration.active;
+    sendStatusUpdate(svcworker);
 
     navigator.serviceWorker.addEventListener("controllerchange", function onController(){
       svcworker = navigator.serviceWorker.controller;
+      sendStatusUpdate(svcworker);
     });
+
+    navigator.serviceWorker.addEventListener("message", onSWMessage);
+  }
+
+  function onSWMessage(evt) {
+    var { data } = evt;
+    if (data.requestStatusUpdate) {
+      console.log("Received status update request from service worker, responding...");
+      sendStatusUpdate(evt.ports && evt.ports[0]);
+    }
+  }
+
+  function sendStatusUpdate(target) {
+    sendSWMessage({ statusUpdate: { isOnline, isLoggedIn }}, target);
+  }
+
+  function sendSWMessage(msg, target) {
+    if (target) {
+      target.postMessage(msg);
+    }
+    else if(svcworker) {
+      svcworker.postMessage(msg);
+    }
+    else {
+      navigator.serviceWorker.controller.postMessage(msg);
+    }
   }
 
 })();
--- exploring_service_workers/service-workers/service-worker-exercise/web/js/sw.js
+++ exploring_service_workers/service-workers/service-worker-exercise/web/js/sw.js
@@ -1,3 +1,28 @@
 "use strict";
 
-// TODO
+const version = 1;
+
+self.addEventListener("install", onInstall);
+self.addEventListener("activate", onActivate);
+
+main().catch(console.error);
+
+// ************************************
+
+async function main() {
+  console.log(`Service Worker (${version}) is starting...`);
+}
+
+async function onInstall(evt) {
+  console.log(`Service Worker (${version}) installed...`);
+  self.skipWaiting();
+}
+
+function onActivate(evt) {
+  evt.waitUntil(handleActivation());
+}
+
+async function handleActivation() {
+  await clients.claim();
+  console.log(`Service Worker (${version}) activated...`);
+}
