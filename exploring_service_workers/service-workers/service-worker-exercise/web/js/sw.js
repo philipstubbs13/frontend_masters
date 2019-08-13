efe3a94238eb4dbf89935325aa255d3c7261fe38
philipstubbs13 <philipstubbs13@gmail.com>
2019-08-13 18:48:33 -0500

    service workers

--- exploring_service_workers/service-workers/service-worker-exercise/web/js/sw.js
+++ exploring_service_workers/service-workers/service-worker-exercise/web/js/sw.js
@@ -1,21 +1,64 @@
 "use strict";
 
-const version = 1;
+const version = 3;
+var isOnline = true;
+var isLoggedIn = false;
+var cacheName = `ramblings-${version}`;
+
+var urlsToCache = {
+  loggedOut: [
+    "/",
+    "/about",
+    "/contact",
+    "/login",
+    "/404",
+    "/offline",
+    "/js/blog.js",
+    "/js/home.js",
+    "/js/login.js",
+    "/js/add-post.js",
+    "/css/style.css",
+    "/images/logo.gif",
+    "/images/offline.png"
+  ]
+};
 
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivate);
+self.addEventListener("message", onMessage);
 
 main().catch(console.error);
 
 // ************************************
 
 async function main() {
-  console.log(`Service Worker (${version}) is starting...`);
+  await sendMessage({ requestStatusUpdate: true });
+  await cacheLoggedOutFiles();
+
+  // console.log(`Service Worker (${version}) is starting...`);
 }
 
 async function onInstall(evt) {
   console.log(`Service Worker (${version}) installed...`);
   self.skipWaiting();
+}
+
+async function sendMessage(msg) {
+  var allClients = await clients.matchAll({ includeUncontrolled: true });
+  return Promise.all(
+    allClients.map(function clientMsg(client){
+      var chan = new MessageChannel();
+      chan.port1.onmessage = onMessage;
+      return client.postMessage(msg, [chan.port2]);
+    })
+  )
+}
+
+function onMessage({ data }) {
+  if(data.statusUpdate) {
+    ({ isOnline, isLoggedIn } = data.statusUpdate);
+    console.log(`Service Worker (v${version}) status update, isOnline:${isOnline}, isLoggedIn:${isLoggedIn}`);
+  }
 }
 
 function onActivate(evt) {
@@ -24,5 +67,37 @@ function onActivate(evt) {
 
 async function handleActivation() {
   await clients.claim();
+  await cacheLoggedOutFiles(/*forceReload=*/true);
   console.log(`Service Worker (${version}) activated...`);
+}
+
+async function cacheLoggedOutFiles(forceReload = false) {
+  var cache = await caches.open(cacheName);
+
+  return Promise.all(
+    urlsToCache.loggedOut.map(async function requestFile(url){
+      try {
+        let res;
+
+        if(!forceReload) {
+          res = await cache.match(url);
+          if (res) {
+            return res;
+          }
+        }
+
+        let fetchOptions = {
+          method: "GET",
+          cache: "no-cache",
+          credentials: "omit",
+        };
+        res = await fetch(url, fetchOptions);
+        if (res.ok) {
+          await cache.put(url, res);
+        }
+      
+      }
+      catch(err) {}
+    })
+  );
 }
