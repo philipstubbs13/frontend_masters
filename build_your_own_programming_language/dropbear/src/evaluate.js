74e7644346cfe36d9bb804bcc9ee03f5601c64db
philipstubbs13 <philipstubbs13@gmail.com>
2019-10-13 20:10:34 -0500

    evaluating expressions

--- build_your_own_programming_language/dropbear/src/evaluate.js
+++ build_your_own_programming_language/dropbear/src/evaluate.js
@@ -1,6 +1,18 @@
 const { environment } = require('./standard-library');
 const last = collection => collection[collection.length - 1];
 
-const evaluate = () => {};
+const apply = node => {
+  const fn = environment[node.name];
+  const args = node.arguments.map(evaluate);
+  if (typeof fn !== 'function') {
+    throw new TypeError(`${node.name} is not a function`);
+  }
+  return fn(...args);
+};
+
+const evaluate = node => {
+  if (node.type === 'CallExpression') return apply(node);
+  if (node.value) return node.value;
+};
 
 module.exports = { evaluate };
