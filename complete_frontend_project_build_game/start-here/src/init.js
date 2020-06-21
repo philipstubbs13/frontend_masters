5582b2ff28ec2af9955f9939636793d277f98d75
philipstubbs13 <philipstubbs13@gmail.com>
2020-06-21 14:00:53 -0500

    state machine

--- complete_frontend_project_build_game/start-here/src/init.js
+++ complete_frontend_project_build_game/start-here/src/init.js
@@ -1,8 +1,6 @@
-const TICK_RATE = 3000;
+import game from './gameState';
 
-function tick() {
-  console.log('tick', Date.now());
-}
+const TICK_RATE = 3000;
 
 async function init() {
   console.log('starting game');
@@ -13,7 +11,7 @@ async function init() {
     const now = Date.now();
 
     if (nextTimeToTick <= now) {
-      tick();
+      game.tick();
       nextTimeToTick = now + TICK_RATE;
     }
 
