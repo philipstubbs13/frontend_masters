53a8abdd327da0529b381c23c699838b584653aa
philipstubbs13 <philipstubbs13@gmail.com>
2020-06-23 23:01:11 -0500

    transitioning between states

--- complete_frontend_project_build_game/start-here/src/constants.js
+++ complete_frontend_project_build_game/start-here/src/constants.js
@@ -2,3 +2,8 @@ export const TICK_RATE = 3000;
 export const ICONS = ["fish", "poop", "weather"];
 export const RAIN_CHANCE = 0.2;
 export const SCENES = ["day", "rain"];
+export const DAY_LENGTH = 60;
+export const NIGHT_LENGTH = 4;
+
+export const getNextHungerTime = clock => Math.floor(Math.random() * 3) + 5 + clock;
+export const getNextDieTime = clock => Math.floor(Math.random() * 2) + 3 + clock;
--- complete_frontend_project_build_game/start-here/src/gameState.js
+++ complete_frontend_project_build_game/start-here/src/gameState.js
@@ -1,16 +1,25 @@
 import { modFox, modScene } from "./ui";
-import { RAIN_CHANCE, SCENES } from './constants';
+import { RAIN_CHANCE, SCENES, DAY_LENGTH, NIGHT_LENGTH, getNextDieTime, getNextHungerTime } from './constants';
 
 const gameState = {
   current: "INIT",
   clock: 1,
   wakeTime: -1,
+  sleepTime: -1,
+  hungryTime: -1,
+  dieTime: -1,
   tick() {
     this.clock++;
     console.log('clock', this.clock);
 
     if (this.clock === this.wakeTime) {
       this.wake();
+    } else if (this.clock === this.sleepTime) {
+      this.sleep();
+    } else if (this.clock === this.hungryTime) {
+      this.getHungry();
+    } else if (this.clock === this.dieTime) {
+      this.die();
     }
 
     return this.clock;
@@ -27,6 +36,23 @@ const gameState = {
     modFox("idling");
     this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
     modScene(SCENES[this.scene]);
+    this.sleepTime = this.clock + DAY_LENGTH;
+    this.hungryTime = getNextHungerTime(this.clock);
+  },
+  sleep() {
+    this.state = 'SLEEP';
+    modFox('sleep');
+    modScene('night');
+    this.wakeTime = this.clock + NIGHT_LENGTH;
+  },
+  getHungry() {
+    this.current = 'HUNGRY';
+    this.dieTime = getNextDieTime(this.clock);
+    this.hungryTime = -1;
+    modFox('hungry');
+  },
+  die() {
+    console.log('die');
   },
   handleUserAction(icon) {
     if (["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)) {
@@ -58,7 +84,15 @@ const gameState = {
     console.log('cleanUpPoop');
   },
   feed() {
-    console.log('feed');
+    if (this.current !== 'HUNGRY') {
+      return;
+    }
+
+    this.current = 'FEEDING';
+    this.dieTime = -1;
+    this.poopTime = getNextPoopTime(this.clock);
+    modFox('eating');
+    this.timeToStartCelebrating = this.clock + 2;
   }
 };
 
