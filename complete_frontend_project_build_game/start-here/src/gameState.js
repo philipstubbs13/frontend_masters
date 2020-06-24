40a168fb528689d5db52292968caa90a654245af
philipstubbs13 <philipstubbs13@gmail.com>
2020-06-23 22:01:31 -0500

    adding helper functions

--- complete_frontend_project_build_game/start-here/src/gameState.js
+++ complete_frontend_project_build_game/start-here/src/gameState.js
@@ -1,3 +1,6 @@
+import { modFox, modScene } from "./ui";
+import { RAIN_CHANCE, SCENES } from './constants';
+
 const gameState = {
   current: "INIT",
   clock: 1,
@@ -5,17 +8,25 @@ const gameState = {
   tick() {
     this.clock++;
     console.log('clock', this.clock);
+
+    if (this.clock === this.wakeTime) {
+      this.wake();
+    }
+
     return this.clock;
   },
   startGame() {
-    console.log("hatching");
     this.current = "HATCHING";
     this.wakeTime = this.clock + 3;
+    modFox('egg');
+    modScene('day');
   },
   wake() {
-    console.log("awoken");
     this.current = "IDLING";
     this.wakeTime = -1;
+    modFox("idling");
+    this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
+    modScene(SCENES[this.scene]);
   },
   handleUserAction(icon) {
     if (["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)) {
