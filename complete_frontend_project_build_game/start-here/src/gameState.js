224fdb08c18109cecf8b1f63bd5035a72a22fbad
philipstubbs13 <philipstubbs13@gmail.com>
2020-06-23 21:40:23 -0500

    addin new states

--- complete_frontend_project_build_game/start-here/src/gameState.js
+++ complete_frontend_project_build_game/start-here/src/gameState.js
@@ -1,14 +1,55 @@
 const gameState = {
   current: "INIT",
   clock: 1,
+  wakeTime: -1,
   tick() {
     this.clock++;
     console.log('clock', this.clock);
     return this.clock;
   },
+  startGame() {
+    console.log("hatching");
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+  },
+  wake() {
+    console.log("awoken");
+    this.current = "IDLING";
+    this.wakeTime = -1;
+  },
   handleUserAction(icon) {
-    console.log(icon);
+    if (["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)) {
+      // do nothing
+      return;
+    }
+
+    if (this.current === 'INIT' || this.current === 'DEAD') {
+      this.startGame();
+      return;
+    }
+
+    switch(icon) {
+      case "weather":
+        this.changeWeather();
+        break;
+      case "poop":
+        this.cleanUpPoop();
+        break;
+      case "fish":
+        this.feed();
+        break;
+    }
+  },
+  changeWeather() {
+    console.log('changeWeather');
+  },
+  cleanUpPoop() {
+    console.log('cleanUpPoop');
+  },
+  feed() {
+    console.log('feed');
   }
 };
 
+export const handleUserAction = gameState.handleUserAction.bind(gameState);
 export default gameState;
--- complete_frontend_project_build_game/start-here/src/init.js
+++ complete_frontend_project_build_game/start-here/src/init.js
@@ -1,10 +1,10 @@
-import game from './gameState';
+import game, { handleUserAction } from './gameState';
 import { TICK_RATE } from './constants';
 import initButtons from './buttons';
 
 async function init() {
   console.log('starting game');
-  initButtons(game.handleUserAction);
+  initButtons(handleUserAction);
 
   let nextTimeToTick = Date.now();
 
