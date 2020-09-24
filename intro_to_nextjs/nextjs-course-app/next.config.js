8dfd8e2ca8e40e43b9fc54e4c30bc8b9daf4d986
philipstubbs13 <philipstubbs13@gmail.com>
2020-09-24 05:00:14 -0500

    plugins

--- intro_to_nextjs/nextjs-course-app/next.config.js
+++ intro_to_nextjs/nextjs-course-app/next.config.js
@@ -1,9 +1,7 @@
-const { PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
 
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    console.log('Im in dev mode')
+dotenvLoad()
 
-    return defaultConfig
-  }
-}
+const withNextEnv = nextEnv()
+module.exports = withNextEnv()
