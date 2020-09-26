6172858bb384fa3e89a28b9b8fb6f18801a39fcd
philipstubbs13 <philipstubbs13@gmail.com>
2020-09-26 09:06:55 -0500

    dynamic rendering

--- intro_to_nextjs/nextjs-course-app/pages/index.jsx
+++ intro_to_nextjs/nextjs-course-app/pages/index.jsx
@@ -4,10 +4,22 @@ import { jsx } from 'theme-ui'
 import Link from 'next/link'
 
 
-export default () => (
+export default ({ content }) => (
   <div sx={{ height: `calc(100vh - 60px)` }}>
     <div sx={{ variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%' }}>
-      <h1 sx={{ fontSize: 8, my: 0 }}>This is a really dope note taking app.</h1>
+      <h1 sx={{ fontSize: 8, my: 0 }}>{content.title}</h1>
     </div>
   </div>
 )
+
+export function getStaticProps() {
+  // get data from CMS
+
+  return {
+    props: {
+      content: {
+        title: 'This is my really nice app'
+      }
+    }
+  }
+}
--- intro_to_nextjs/nextjs-course-app/pages/notes/[id].jsx
+++ intro_to_nextjs/nextjs-course-app/pages/notes/[id].jsx
@@ -4,13 +4,35 @@ import { jsx } from 'theme-ui'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-export default () => {
-  const router = useRouter()
-  const { id } = router.query
+export default ({ note }) => {
+  // const router = useRouter()
+  // const { id } = router.query
 
   return (
     <div sx={{ variant: 'containers.page' }}>
-      <h1>Note: {id} </h1>
+      <h1>Note: {note.title} </h1>
     </div>
   )
+}
+
+export async function getServerSideProps({ params, req, res  }) {
+  const response = await fetch(`http://localhost:3000/api/note/${params.id}`)
+
+  if (!response.ok) {
+    res.writeHead(302, {
+      Location: '/notes'
+    })
+
+    res.end()
+
+    return {
+      props: {}
+    }
+  }
+
+  const { data } = await response.json()
+
+  return {
+    props: { note: data }
+  }
 }
