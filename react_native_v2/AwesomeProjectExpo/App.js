cde10a17e32de5a5c065ddac18ca1a161455d999
philipstubbs13 <philipstubbs13@gmail.com>
2020-11-12 21:36:11 -0600

    styling

--- react_native_v2/AwesomeProjectExpo/App.js
+++ react_native_v2/AwesomeProjectExpo/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, styles.pink]}>
         <Text>Hello, world!</Text>
       </View>
     </SafeAreaView>
   )
 };
+
+const styles = StyleSheet.create({
+  pink: {
+    backgroundColor: 'pink',
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  safeArea: {
+    flex: 2
+  }
+});
 
 export default App;
