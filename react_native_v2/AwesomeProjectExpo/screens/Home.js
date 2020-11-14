8e9b25177b618efdb39965eb3ba2c437e275353c
philipstubbs13 <philipstubbs13@gmail.com>
2020-11-14 15:05:16 -0600

    network requests

--- react_native_v2/AwesomeProjectExpo/screens/Home.js
+++ react_native_v2/AwesomeProjectExpo/screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 import PalettePreview from '../components/PalettePreview';
 
@@ -44,10 +44,25 @@ const COLOR_PALETTES = [
 ];
 
 const Home = ({ navigation }) => {
+  const [colorPalettes, setColorPalettes] = useState([]);
+
+  const fetchColorPalettes = useCallback(async () => {
+    const result = await fetch('https://color-palette-api.kadikraman.now.sh/palettes');
+
+    if (result.ok) {
+      const palettes = await result.json();
+      setColorPalettes(palettes);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchColorPalettes();
+  }, [])
+
   return (
     <FlatList
       style={styles.list}
-      data={COLOR_PALETTES}
+      data={colorPalettes}
       keyExtractor={item => item.paletteName}
       renderItem={({ item}) => (
         <PalettePreview
