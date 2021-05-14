42f902d9787711fc4e50ebe1cd76b12b0708a20a
philipstubbs13 <philipstubbs13@gmail.com>
2021-05-14 17:25:46 -0500

    intro to react

---
+++ complete_intro_to_react_v6/README.md
@@ -0,0 +1,6 @@
+# Complete Intro to React V6
+
+* <https://frontendmasters.com/courses/complete-react-v6/>
+* <https://btholt.github.io/complete-intro-to-react-v6/>
+* <https://github.com/btholt/complete-intro-to-react-v6>
+* <https://github.com/btholt/citr-v6-project/>
---
+++ complete_intro_to_react_v6/adopt-me/src/App.js
@@ -0,0 +1,22 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed)
+  ])
+}
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+      React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
+      React.createElement(Pet,  { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
+      React.createElement(Pet,  { name: "Sudo", animal: "Dog", breed: "Wheaten Terrier" }),
+    ]
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
