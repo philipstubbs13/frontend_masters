59b907079bd7defeb6b44e73ba7de33cbe6b68de
philipstubbs13 <philipstubbs13@gmail.com>
2021-06-09 05:12:42 -0500

    proptypes

--- react_and_typescript/react-and-typescript-projects-master/examples/01-nametag/src/Application.tsx
+++ react_and_typescript/react-and-typescript-projects-master/examples/01-nametag/src/Application.tsx
@@ -1,4 +1,10 @@
-const NameTag = () => {
+import * as PropTypes from 'prop-types';
+
+type NameTagProps = {
+  name: string;
+};
+
+const NameTag = (props: NameTagProps) => {
   return (
     <main>
       <header>
@@ -6,13 +12,13 @@ const NameTag = () => {
         <p>My Name Is</p>
       </header>
       <section className="display-name">
-        <p>[Your Name Here]</p>
+        <p>{props.name}</p>
       </section>
       <footer />
     </main>
   );
 };
 
-const Application = () => <NameTag />;
+const Application = () => <NameTag name="Steve" />;
 
 export default Application;
