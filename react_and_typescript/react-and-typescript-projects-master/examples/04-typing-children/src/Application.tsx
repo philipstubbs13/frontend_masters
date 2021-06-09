d6a157c03ddbb440fa6d890ea44008bfb492481b
philipstubbs13 <philipstubbs13@gmail.com>
2021-06-09 05:37:28 -0500

    typing children

--- react_and_typescript/react-and-typescript-projects-master/examples/04-typing-children/src/Application.tsx
+++ react_and_typescript/react-and-typescript-projects-master/examples/04-typing-children/src/Application.tsx
@@ -8,29 +8,30 @@
  * React.ReactChild[];
  */
 
- type BoxProps = { children: any };
+import React from 'react';
 
- const Box = ({ children }: BoxProps) => {
-   return (
-     <section style={{ padding: "1em", border: "5px solid purple" }}>
-       {children}
-     </section>
-   );
- };
- 
- export default function Application() {
-   return (
-     <Box>
-       Just a string.
-       <p>Some HTML that is not nested.</p>
-       <Box>
-         <h2>Another React component with one child.</h2>
-       </Box>
-       <Box>
-         <h2>A nested React component with two children.</h2>
-         <p>The second child.</p>
-       </Box>
-     </Box>
-   );
- }
- 
+type BoxProps = { children: React.ReactNode };
+
+const Box = ({ children }: BoxProps) => {
+  return (
+    <section style={{ padding: '1em', border: '5px solid purple' }}>
+      {children}
+    </section>
+  );
+};
+
+export default function Application() {
+  return (
+    <Box>
+      Just a string.
+      <p>Some HTML that is not nested.</p>
+      <Box>
+        <h2>Another React component with one child.</h2>
+      </Box>
+      <Box>
+        <h2>A nested React component with two children.</h2>
+        <p>The second child.</p>
+      </Box>
+    </Box>
+  );
+}
