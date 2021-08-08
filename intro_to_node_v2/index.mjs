4371f26c521eabb8a6e8ca223252bf9b870dcccf
philipstubbs13 <philipstubbs13@gmail.com>
2021-08-08 11:45:27 -0500

    file system

--- intro_to_node_v2/index.mjs
+++ intro_to_node_v2/index.mjs
@@ -1,4 +1,15 @@
-export const action = () => {
-  console.log('hello')
+import { readFile, writeFile } from 'fs/promises'
+
+
+let template = await readFile(new URL('template.html', import.meta.url), 'utf-8')
+
+const data = {
+  title: 'Learn Node.js',
+  body: 'This is the final HTML'
 }
 
+for (const [k, v] of Object.entries(data)) {
+  template = template.replace(`{${k}}`, v)
+}
+
+await writeFile(new URL('index.html', import.meta.url), template)
