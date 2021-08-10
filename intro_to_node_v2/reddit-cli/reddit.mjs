8fd1b8708d82e84370f2c49c5dac2322e5fa26b0
philipstubbs13 <philipstubbs13@gmail.com>
2021-08-09 21:57:40 -0500

    building a reddit cli

--- intro_to_node_v2/reddit-cli/reddit.mjs
+++ intro_to_node_v2/reddit-cli/reddit.mjs
@@ -1,3 +1,22 @@
 #! /usr/bin/env node
+import fetch from 'node-fetch'
+import open from 'open'
+import yargs from 'yargs'
 
-console.log('hello from reddit cli')
+const { argv } = yargs(process.argv)      
+
+const res = await fetch('https://reddit.com/.json')
+const data = await res.json()
+const children = data.data.children
+const randomIndex = Math.floor(Math.random() * children.length)
+const randomPost = children[randomIndex]
+const link = `https://reddit.com${randomPost.data.permalink}`
+
+if (argv.print) {
+  console.log(`
+    title: ${randomPost.data.title}
+    link: ${link}
+  `)
+} else {
+  open(link)
+}
