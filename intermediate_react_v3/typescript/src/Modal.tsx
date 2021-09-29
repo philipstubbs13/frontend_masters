1280753e4d9f66eedf34f4fb7c986083b45d7374
philipstubbs13 <philipstubbs13@gmail.com>
2021-09-29 01:32:20 -0500

    typescript

---
+++ intermediate_react_v3/typescript/src/Modal.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef, FunctionComponent, MutableRefObject } from "react";
+import { createPortal } from "react-dom";
+
+const modalRoot = document.getElementById("modal");
+
+const Modal: FunctionComponent = ({ children }) => {
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
+
+    modalRoot.appendChild(elRef.current);
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
