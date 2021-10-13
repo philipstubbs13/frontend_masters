72142347486a972a25d684c093bee906869b22c9
philipstubbs13 <philipstubbs13@gmail.com>
2021-10-13 05:00:23 -0500

    intro to gatsby

--- introduction_to_gatsby_v2/intro-to-gatsby-project/src/pages/index.js
+++ introduction_to_gatsby_v2/intro-to-gatsby-project/src/pages/index.js
@@ -1,9 +1,28 @@
 import * as React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetSiteTitle {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  const meta = data?.site?.siteMetadata ?? {};
+
   return (
-    <main>
-      <h1>Hello Frontend Masters</h1>
-    </main>
+    <>
+      <header>
+        <Link to="/">{meta.title}</Link>
+      </header>
+      <main>
+        <h1>Hello Frontend Masters</h1>
+        <Link to="/about">About this site</Link>
+      </main>
+    </>
   );
 }
