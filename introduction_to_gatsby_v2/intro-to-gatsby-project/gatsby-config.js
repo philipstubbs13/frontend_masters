cbda97bc8d46639b62d5844273c4ed982408bc63
philipstubbs13 <philipstubbs13@gmail.com>
2021-10-15 06:35:48 -0500

    mdx

--- introduction_to_gatsby_v2/intro-to-gatsby-project/gatsby-config.js
+++ introduction_to_gatsby_v2/intro-to-gatsby-project/gatsby-config.js
@@ -6,5 +6,30 @@ module.exports = {
     image:
       'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+
+    // This set of plugins is to enable creating MDX blog posts from the src/posts folder.
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          posts: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
