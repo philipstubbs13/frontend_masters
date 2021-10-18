178153d964e2da29432535c12dcccf1967f9ad36
philipstubbs13 <philipstubbs13@gmail.com>
2021-10-17 21:40:04 -0500

    creating a listing of third party data

--- introduction_to_gatsby_v2/intro-to-gatsby-project/src/pages/about.js
+++ introduction_to_gatsby_v2/intro-to-gatsby-project/src/pages/about.js
@@ -1,13 +1,28 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout.js';
 
-export default function AboutPage() {
+export const query = graphql`
+  query CocktailQuery {
+    file(name: { eq: "cocktail" }) {
+      childImageSharp {
+        gatsbyImageData(placeholder: DOMINANT_COLOR)
+      }
+    }
+  }
+`;
+
+export default function AboutPage({ data }) {
   return (
     <Layout
       title="About this site"
       description="More information about this site."
     >
+      <GatsbyImage
+        image={getImage(data.file)}
+        alt="a cocktail set inside an elaborate..."
+      />
       <h1>About This Site</h1>
       <Link to="/">Back to home</Link>
     </Layout>
