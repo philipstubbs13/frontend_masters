1348095b51ebe26f1c26cc259d3748f303a84d91
philipstubbs13 <philipstubbs13@gmail.com>
2022-01-18 00:06:18 -0600

    building pwas from scratch

--- building_pwas_from_scratch/frontendmasters-pwa/sw-register.js
+++ building_pwas_from_scratch/frontendmasters-pwa/sw-register.js
@@ -7,12 +7,24 @@ const assets = ['/', 'styles.css', 'app.js', 'sw-register.js', 'https://fonts.gs
 
 self.addEventListener("install", event => {
   console.log('cache assets');
-  caches.open('assets').then(cache => {
-    cache.addAll(assets);
-  })
+  event.waitUntil(
+    caches.open('assets').then(cache => {
+      cache.addAll(assets);
+    })
+  )
 })
 
 self.addEventListener("fetch", event => {
-  const response = new Response(`hello, I'm a response on URL ${event.request.url}`)
-  event.respondWith(response)
-})
+    event.respondWith(
+        caches.match(event.request)  // searching in the cache
+            .then( response => {
+                if (response) {
+                    // The request is in the cache 
+                    return response; // cache hit
+                } else {
+                    // We need to go to the network  
+                    return fetch(event.request);  // cache miss
+                }
+            })
+    );
+});
