46d32b94a1e6f1a63900ad30498d35842b1529da
Phil <philipstubbs13@gmail.com>
2022-02-20 08:01:34 -0600

    web sockets

--- intro-to-real-time/realtime-exercises-main/websockets/exercise-raw/backend/server.js
+++ intro-to-real-time/realtime-exercises-main/websockets/exercise-raw/backend/server.js
@@ -24,11 +24,31 @@ const server = http.createServer((request, response) => {
   });
 });
 
-/*
- *
- * your code goes here
- *
- */
+server.on("upgrade", (req, socket) => {
+  if (req.headers["upgrade"] !== 'websocket') {
+    socket.end("HTTP/1.1 400 Bad Request")
+    return
+  }
+
+  const acceptKey = req.headers['sec-websocket-key']
+  const acceptValue = generateAcceptValue(acceptKey)
+  const headers = [
+    "HTTP/1.1 101 Web Socket Protocol Handshake",
+    "Upgrade: WebSocket",
+    "Connection: Upgrade",
+    `Sec-WebSocket-Accept: ${acceptValue}`,
+    "Sec-WebSocket-Protocol: json",
+    "\r\n"
+  ]
+
+  socket.write(headers.join("\r\n"))
+  socket.write(objToResponse({ msg: getMsgs() }))
+
+  socket.on("data", (buffer) => {
+    console.log(buffer);
+  })
+})
+
 
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
--- intro-to-real-time/realtime-exercises-main/websockets/exercise-raw/frontend/raw-chat.js
+++ intro-to-real-time/realtime-exercises-main/websockets/exercise-raw/frontend/raw-chat.js
@@ -11,14 +11,24 @@ chat.addEventListener("submit", function (e) {
 });
 
 async function postNewMsg(user, text) {
-  // code goes here
+  const data = { user, text }
+
+  ws.send(JSON.stringify(data))
 }
 
-/*
- *
- * your code goes here
- *
- */
+const ws = new WebSocket("ws://localhost:8080", ["json"])
+
+ws.addEventListener("open", () => {
+  console.log('connected')
+  presence.innerText = '🟢'
+})
+
+ws.addEventListener("message", (event) => {
+  const data = JSON.parse(event.data);
+  allChat = data.msg
+  render()
+})
+
 
 function render() {
   const html = allChat.map(({ user, text }) => template(user, text));
