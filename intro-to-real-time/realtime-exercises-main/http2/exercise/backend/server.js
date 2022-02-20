593cb52b67eedf5cbb558c77b2bfa955b2f4bd11
Phil <philipstubbs13@gmail.com>
2022-02-20 07:17:57 -0600

    managing http/2 connections

--- intro-to-real-time/realtime-exercises-main/http2/exercise/backend/server.js
+++ intro-to-real-time/realtime-exercises-main/http2/exercise/backend/server.js
@@ -50,9 +50,11 @@ server.on("stream", (stream, headers) => {
 
     // write the first response
     stream.write(JSON.stringify({ msg: getMsgs() }))
+    connections.push(stream)
 
     stream.on("close", () => {
       console.log("disconnected " + stream.id)
+      connections = connections.filter(s => s !== stream)
     })
   }
 })
@@ -75,11 +77,17 @@ server.on("request", async (req, res) => {
     const data = Buffer.concat(buffers).toString();
     const { user, text } = JSON.parse(data);
 
-    /*
-     *
-     * some code goes here
-     *
-     */
+    msg.push({
+      user,
+      text,
+      time: Date.now()
+    })
+
+    res.end()
+
+    connections.forEach((stream) => {
+      stream.write(JSON.stringify({ msg: getMsgs() }))
+    })
   }
 });
 
