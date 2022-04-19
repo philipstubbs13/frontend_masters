6aa4ab46d854f84ba9d840fa9e69666d5052625f
Phil <philipstubbs13@gmail.com>
2022-04-19 04:15:38 -0500

    intro to react v7

--- complete-intro-to-react-v7/adopt-me/src/App.js
+++ complete-intro-to-react-v7/adopt-me/src/App.js
@@ -1,14 +1,21 @@
 import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import SearchParams from './SearchParams';
 import { StrictMode } from 'react';
+import Details from './Details';
 
 const App = () => {
   return (
     <StrictMode>
-      <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
-      </div>
+      <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   )
 }
--- complete-intro-to-react-v7/adopt-me/src/Pet.js
+++ complete-intro-to-react-v7/adopt-me/src/Pet.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Pet = ({ name, animal, breed, images, location, id }) => {
 
   let hero = 'http://pet-images.dev-apis.com/pets/none.jpg';
@@ -7,7 +9,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
 
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -15,7 +17,7 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
         <h1>{name}</h1>
         <h2>{animal} - {breed} - {location}</h2>
       </div>
-    </a>
+    </Link>
   )
 }
 
