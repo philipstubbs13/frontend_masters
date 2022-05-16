06a48fa13262a1436336eefa9378fdb129df40e2
Phil <philipstubbs13@gmail.com>
2022-05-16 05:15:59 -0500

    jquery clone

--- making-typescript-stick/making-typescript-stick-master/challenges/jquery-clone/src/index.ts
+++ making-typescript-stick/making-typescript-stick-master/challenges/jquery-clone/src/index.ts
@@ -1,13 +1,71 @@
 import fetch from "node-fetch";
 
-function $(selector: string): any {
-  return {} as any;
+class SelectorResult {
+  #elements
+  constructor(elements: NodeListOf<Element>) {
+    this.#elements = elements;
+  }
+  html(contents: string) {
+    // iterate over everything we found
+    this.#elements.forEach(elem => {
+      // set contents equal to the string we were given.
+      elem.innerHTML = contents
+    })
+  }
+  on<K extends keyof HTMLElementEventMap>(eventName: K, cb: (event: HTMLElementEventMap[K]) => void) {
+    this.#elements.forEach(elem => {
+      // would want a type guard to check wheter something is
+      // an HTML element (vs a Element)
+      const htmlElem = elem as HTMLElement;
+      htmlElem.addEventListener(eventName, cb);
+    })
+  }
+  show() {
+    this.#elements.forEach(elem => {
+      // would want a type guard to check wheter something is
+      // an HTML element (vs a Element)
+      const htmlElem = elem as HTMLElement;
+      htmlElem.style.visibility = 'visible';
+    })
+  }
+  hide() {
+    this.#elements.forEach(elem => {
+      // would want a type guard to check wheter something is
+      // an HTML element (vs a Element)
+      const htmlElem = elem as HTMLElement;
+      htmlElem.style.visibility = 'hidden';
+    })
+  }
+}
+
+function $(selector: string) {
+  return new SelectorResult(
+    document.querySelectorAll(selector)
+  )
 }
 
 namespace $ {
-  export function ajax(...args: any[]): any {
-    return {} as any;
+  export function ajax({url, successCb }  : { url: string, successCb: (data: any) => void }): any {
+    return fetch(url)
+      .then(resp => resp.json())
+      .then(successCb)
   }
 }
 
 export default $;
+
+$("button.continue").html("Next Step...")
+
+const hiddenBox = $("#banner-message")
+$("#button-container button").on("click", (event) => {
+  hiddenBox.show()
+})
+
+$.ajax({
+  url: "https://jsonplaceholder.typicode.com/posts/33",
+  success: (result) => {
+    $("#post-info").html(
+      "<strong>" + result.title + "</strong>" + result.body
+    )
+  },
+})
