cdb85ca03791e3bbcbe13728d5a9d8095fad02dd
Phil <philipstubbs13@gmail.com>
2022-11-08 14:06:56 -0600

    api design in nodejs

--- api-design-nodejs-v4/course/src/router.ts
+++ api-design-nodejs-v4/course/src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -9,7 +10,14 @@ router.get('/product', (req, res) => {
     res.json({ message: 'hello' })
 })
 router.get('/product/:id', () => {})
-router.put('/product/:id', () => {})
+router.put('/product/:id', body('name').isString(), (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(400);
+        res.json({ errors: errors.array() });
+    }
+})
 router.post('/product', () => {})
 router.delete('/update/:id', () => {})
 
