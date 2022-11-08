e70a4d16d2cf02bfc09176ad476a5251f90a2982
Phil <philipstubbs13@gmail.com>
2022-11-08 07:46:10 -0600

    api design in nodejs

--- api-design-nodejs-v4/course/src/server.ts
+++ api-design-nodejs-v4/course/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './router';
 import morgan from 'morgan';
 import cors from 'cors';
+import { protect } from './modules/auth';
 
 const app = express();
 
@@ -16,6 +17,6 @@ app.get('/', (req, res) => {
     res.json({ message: 'hello' })
 })
 
-app.use('/api', router)
+app.use('/api', protect, router)
 
 export default app
