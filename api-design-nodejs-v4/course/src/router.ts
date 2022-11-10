cc197b0aa789c33a6b343ffcc7d714972cc2bd36
Phil <philipstubbs13@gmail.com>
2022-11-10 05:11:40 -0600

    api design in nodejs

--- api-design-nodejs-v4/course/src/router.ts
+++ api-design-nodejs-v4/course/src/router.ts
@@ -1,44 +1,54 @@
 import { Router } from 'express';
-import { body, validationResult } from "express-validator";
+import { body, oneOf, validationResult } from "express-validator";
+import { createProduct, deleteProduct, getOneProduct, getProducts } from './handlers/product';
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from './handlers/update';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
 /**
  * Product
  */
-router.get('/product', (req, res) => {
-    res.json({ message: 'hello' })
-})
-router.get('/product/:id', () => {})
-router.put('/product/:id', body('name').isString(), (req, res) => {
-    const errors = validationResult(req);
+router.get('/product', getProducts)
+router.get('/product/:id', getOneProduct)
+router.put('/product/:id', body('name').isString(), handleInputErrors, (req, res) => {
 
-    if (!errors.isEmpty()) {
-        res.status(400);
-        res.json({ errors: errors.array() });
-    }
 })
-router.post('/product', () => {})
-router.delete('/update/:id', () => {})
+router.post('/product', body('name').isString(), handleInputErrors, createProduct)
+router.delete('/product/:id', deleteProduct)
 
 /**
  * Update
  */
- router.get('/update', (req, res) => {
-    res.json({ message: 'hello' })
- })
- router.get('/update/:id', () => {})
- router.put('/update/:id', () => {})
- router.post('/update', () => {})
- router.delete('/update/:id', () => {})
+ router.get('/update', getUpdates)
+ router.get('/update/:id', getOneUpdate)
+ router.put('/update/:id',
+    body('title').optional(),
+    body('body').optional(),
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+    body('version').optional(),
+    updateUpdate)
+router.post('/update',
+    body('title').exists().isString(),
+    body('body').exists().isString(),
+    body('productId').exists().isString(),
+    createUpdate)
+ router.delete('/update/:id', deleteUpdate)
 
  /**
   * Update Point
   */
   router.get('/updatepoint', () => {})
   router.get('/updatepoint/:id', () => {})
-  router.put('/updatepoint/:id', () => {})
-  router.post('/updatepoint/:id', () => {})
+  router.put('/updatepoint/:id',
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+    () => {})
+  router.post('/updatepoint',
+    body('name').isString(),
+    body('description').isString(),
+    body('updateId').exists().isString(),
+    () => {})
   router.delete('/updatepoint/:id', () => {})
 
   export default router;
