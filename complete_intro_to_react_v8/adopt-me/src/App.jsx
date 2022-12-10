511c5d0c3f58246fc240e40d46155f8c3f7c9a19
Phil <philipstubbs13@gmail.com>
2022-12-10 15:52:17 -0600

    complete intro to react

--- complete_intro_to_react_v8/adopt-me/src/App.jsx
+++ complete_intro_to_react_v8/adopt-me/src/App.jsx
@@ -1,14 +1,14 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="dog" breed="Havanese" />
-    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-    <Pet name="Doink" animal="cat" breed="Mixed" />
-  </div>
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+      </div>
+    )
 };
 
 const container = document.getElementById("root");
