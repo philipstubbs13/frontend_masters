509300637a853728371c55244654ff4e16efece1
Phil <philipstubbs13@gmail.com>
2022-12-10 07:29:19 -0600

    complete intro to react

--- complete_intro_to_react_v8/adopt-me/src/App.jsx
+++ complete_intro_to_react_v8/adopt-me/src/App.jsx
@@ -1,35 +1,16 @@
-import React from "react";
 import { createRoot } from "react-dom";
+import Pet from "./Pet";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h1", {}, props.animal),
-    React.createElement("h1", {}, props.breed),
-  ]);
-};
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Pepper", 
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Doink",
-      breed: "Mixed",
-    }),
-  ]);
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="dog" breed="Havanese" />
+    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="cat" breed="Mixed" />
+  </div>
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
