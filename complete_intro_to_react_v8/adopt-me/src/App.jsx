0189d10093614cdf4428cb227b37822906be3786
Phil <philipstubbs13@gmail.com>
2022-12-11 08:52:56 -0600

    complete intro to react

--- complete_intro_to_react_v8/adopt-me/src/App.jsx
+++ complete_intro_to_react_v8/adopt-me/src/App.jsx
@@ -1,14 +1,33 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; 
+import Details from "./Details";
 import SearchParams from "./SearchParams";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    }
+  }
+})
 
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-      </div>
-    )
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
+  )
 };
 
 const container = document.getElementById("root");
--- complete_intro_to_react_v8/adopt-me/src/SearchParams.jsx
+++ complete_intro_to_react_v8/adopt-me/src/SearchParams.jsx
@@ -1,40 +1,40 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import Results from "./Results";
 import useBreedList from "./useBreedList";
+import fetchSearch from "./fetchSearch";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("");
+    const [requestParams, setRequestParams] = useState({
+        location: "",
+        animal: "",
+        breed: "",
+    })
     const [animal, setAnimal] = useState("");
-    const [breed, setBreed] = useState("");
-    const [pets, setPets] = useState([]);
     const [breeds] = useBreedList(animal);
 
-    useEffect(() => {
-        requestPets();
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
-
-    async function requestPets() {
-        const res = await fetch(
-            `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-        );
-        const json = await res.json();
-
-        setPets(json.pets);
-    }
+    const results = useQuery(["search", requestParams], fetchSearch);
+    const pets = results?.data?.pets ?? [];
 
     return (
         <div className="search-params">
             <form onSubmit={e => {
                 e.preventDefault();
-                requestPets();
+                const formData = new FormData(e.target);
+                const obj = {
+                    animal: formData.get("animal") ?? "",
+                    breed: formData.get("breed") ?? "",
+                    location: formData.get("location") ?? "",
+                };
+                setRequestParams(obj);
+
             }}>
                 <label htmlFor="location">
                     Location
                     <input
-                        onChange={e => setLocation(e.target.value)}
+                        name="location"
                         id="location"
-                        value={location}
                         placeholder="Location"
                     />
                 </label>
@@ -45,7 +45,6 @@ const SearchParams = () => {
                         value={animal}
                         onChange={e => {
                             setAnimal(e.target.value)
-                            setBreed("")
                         }}
                     >
                         <option />
@@ -59,10 +58,7 @@ const SearchParams = () => {
                     <select
                         id="breed"
                         disabled={breeds.length === 0}
-                        value={breed}
-                        onChange={e => {
-                            setBreed(e.target.value)
-                        }}
+                        name="breed"
                     >
                         <option />
                         {breeds.map(breed => (
