ca95a90863eb6f9c961f6677c5ff4e6cca16a403
Phil <philipstubbs13@gmail.com>
2022-12-17 07:32:39 -0600

    server side rendering

---
+++ intermediate_react_v5/server-side-rendering/src/App.jsx
@@ -0,0 +1,36 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  const adoptedPet = useState(null);
+  return (
+    <div>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
+    </div>
+  );
+};
+
+export default App;
