3f08c699885f879ba0b9509e6a960d94030be219
Phil <philipstubbs13@gmail.com>
2022-12-18 18:26:57 -0600

    redux

---
+++ intermediate_react_v5/redux/src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import adoptedPet from './adoptedPetSlice';
+
+const store = configureStore({
+    reducer: {
+        adoptedPet,
+    }
+});
+
+export default store;
