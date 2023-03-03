385169d6f3d484b4eaa47b267c072e371a3da366
Phil <philipstubbs13@gmail.com>
2023-03-02 19:15:54 -0600

    full stack app with nextjs

--- build-fullstack-app-nextjs-v2/project-app/app/(dashboard)/layout.tsx
+++ build-fullstack-app-nextjs-v2/project-app/app/(dashboard)/layout.tsx
@@ -1,12 +1,14 @@
 import '@/styles/global.css';
 import GlassPane from "@/components/GlassPane";
+import Sidebar from '@/components/Sidebar';
 
 export default function DashboardRootLayout({ children }) {
     return (
         <html lang="en">
             <head />
-            <body className="h-screen w-screen rainbow-mesh p-6">
-                <GlassPane className="w-full h-full flex items-center justify-center">
+            <body className="h-screen w-screen candy-mesh p-6">
+                <GlassPane className="w-full h-full flex items-center">
+                    <Sidebar />
                     {children}
                 </GlassPane>
             </body>
