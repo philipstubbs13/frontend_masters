e688a99d20032a0628c122a26d62a1416529e09d
philipstubbs13 <philipstubbs13@gmail.com>
2023-09-04 11:57:48 -0500

    intro to node

--- intro-to-nodejs-v3/intro-node/src/command.js
+++ intro-to-nodejs-v3/intro-node/src/command.js
@@ -1,6 +1,7 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import { findNotes, getAllNotes, newNote, removeAllNotes, removeNote } from './notes.js'
+import { start } from './server.js'
 
 const listNotes = notes => {
   notes.forEach(({ id, content, tags }) => {
@@ -57,7 +58,8 @@ yargs(hideBin(process.argv))
           type: 'number'
         })
     }, async (argv) => {
-
+      const notes = await getAllNotes()
+      start(notes, argv.port)
     })
     .command('clean', 'remove all notes', () => {}, async (argv) => {
       await removeAllNotes();
--- intro-to-nodejs-v3/intro-node/tests/notes.test.js
+++ intro-to-nodejs-v3/intro-node/tests/notes.test.js
@@ -1,7 +1,52 @@
-const add = (num, num2) => num + num2
+import { jest } from '@jest/globals';
 
-test('add takes two numbers and returns a sum', () => {
-    const result = add(1, 2)
+jest.unstable_mockModule('../src/db.js', () => ({
+  insertDB: jest.fn(),
+  getDB: jest.fn(),
+  saveDB: jest.fn(),
+}));
 
-    expect(result).toBe(3);
+const { insertDB, getDB, saveDB } = await import('../src/db.js');
+const { newNote, getAllNotes, removeNote } = await import('../src/notes.js');
+
+beforeEach(() => {
+  insertDB.mockClear();
+  getDB.mockClear();
+  saveDB.mockClear();
 })
+
+test('newNote inserts data and returns it', async () => {
+    const note = {
+        content: 'this is my note',
+        id: 1,
+        tags: ['hello']
+    }
+    insertDB.mockResolvedValue(note);
+
+    const result = await newNote(note.content, note.tags)
+    expect(result.content).toEqual(note.content)
+    expect(result.tags).toEqual(note.tags)
+})
+
+test('getAllNotes returns all notes', async () => {
+    const db = {
+      notes: ['note1', 'note2', 'note3']
+    };
+    getDB.mockResolvedValue(db);
+  
+    const result = await getAllNotes();
+    expect(result).toEqual(db.notes);
+  });
+  
+  test('removeNote does nothing if id is not found', async () => {
+    const notes = [
+      { id: 1, content: 'note 1' },
+      { id: 2, content: 'note 2' },
+      { id: 3, content: 'note 3' },
+    ];
+    saveDB.mockResolvedValue(notes);
+  
+    const idToRemove = 4;
+    const result = await removeNote(idToRemove);
+    expect(result).toBeUndefined();
+  });
