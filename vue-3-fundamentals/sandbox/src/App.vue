daea96b4f59864375a00fd24e6ff5ac892eeebed
philipstubbs13 <philipstubbs13@gmail.com>
2023-11-13 04:07:21 -0600

    vue 3 fundamentals

--- vue-3-fundamentals/sandbox/src/App.vue
+++ vue-3-fundamentals/sandbox/src/App.vue
@@ -1,51 +1,21 @@
 <script>
+import BaseCounter from './components/BaseCounter.vue'
+
 export default {
+  components: {
+    BaseCounter,
+  },
   data() {
     return {
-      count: 10,
-      counterTitle: 'Counter Standard',
-      incrementAmount: 8,
       message: 'Hello it works',
       listOfNumbers: [1, 2, 3, 4, 5]
     }
   },
-  computed: {
-    displayTitle() {
-      if (this.count > 20) {
-        return 'Counter Standard - Very Long'
-      } else {
-        return 'Counter Standard'
-      }
-    },
-    optimizedIncrementAmount() {
-      return this.displayTitle.length * this.incrementAmount
-    }
-  },
-  methods: {
-    incrementCount() {
-      this.count += this.optimizedIncrementAmount
-    }
-  },
-  watch: {
-    count(newValue) {
-      if (newValue > 20) {
-        this.counterTitle += ' Very Long'
-      }
-    }
-  }
 }
 </script>
 
 <template>
-  <h1>{{ displayTitle }}</h1>
-  <p>{{ count }}</p>
-  <button v-on:click="incrementCount">Increment Count</button>
-  <h1>{{ incrementAmount }}</h1>
-  <p>{{ optimizedIncrementAmount }}</p>
-  <div>
-    <label for="incrementAmount">Increment by:</label>
-    <input type="number" v-model="incrementAmount" />
-  </div>
+  <base-counter />
   <hr />
   <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
   <p v-else>Odd: {{ message }}</p>
---
+++ vue-3-fundamentals/sandbox/src/components/BaseCounter.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    data: () => ({
+        count: 10,
+        counterTitle: 'Counter Standard',
+        incrementAmount: 8,
+    }),
+    computed: {
+        displayTitle() {
+            if (this.count > 20) {
+                return 'Counter Standard - Very Long'
+            } else {
+                return 'Counter Standard'
+            }
+        },
+        optimizedIncrementAmount() {
+            return this.displayTitle.length * this.incrementAmount
+        }
+    },
+    methods: {
+        incrementCount() {
+            this.count += this.optimizedIncrementAmount
+        }
+    },
+}
+</script>
+
+<template>
+    <h1>{{ displayTitle }}</h1>
+    <p>{{ count }}</p>
+    <button v-on:click="incrementCount">Increment Count</button>
+    <h1>{{ incrementAmount }}</h1>
+    <p>{{ optimizedIncrementAmount }}</p>
+    <div>
+        <label for="incrementAmount">Increment by:</label>
+        <input type="number" v-model="incrementAmount" />
+    </div>
+</template>
