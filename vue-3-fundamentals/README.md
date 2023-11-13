c6d19a481e528d309617a70c4cbdda98db74bb24
philipstubbs13 <philipstubbs13@gmail.com>
2023-11-12 20:17:27 -0600

    vue 3 fundamentals

--- vue-3-fundamentals/README.md
+++ vue-3-fundamentals/README.md
@@ -2,3 +2,7 @@
 
 - <https://frontendmasters.com/courses/vue-fundamentals/>
 - course repo: <https://github.com/bencodezen/complete-intro-to-vue-3-workshop>
+* <https://www.shopify.com/partners/blog/what-is-progressive-enhancement-and-why-should-you-care>
+* <https://www.smashingmagazine.com/2018/02/jquery-vue-javascript/>
+* <https://github.com/vuejs/create-vue>
+* <https://chrome.google.com/webstore/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd?hl=en>
---
+++ vue-3-fundamentals/sandbox/src/App.vue
@@ -0,0 +1,86 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 10,
+      counterTitle: 'Counter Standard',
+      incrementAmount: 8,
+      message: 'Hello it works',
+      listOfNumbers: [1, 2, 3, 4, 5]
+    }
+  },
+  computed: {
+    displayTitle() {
+      if (this.count > 20) {
+        return 'Counter Standard - Very Long'
+      } else {
+        return 'Counter Standard'
+      }
+    },
+    optimizedIncrementAmount() {
+      return this.displayTitle.length * this.incrementAmount
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.count += this.optimizedIncrementAmount
+    }
+  },
+  watch: {
+    count(newValue) {
+      if (newValue > 20) {
+        this.counterTitle += ' Very Long'
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ displayTitle }}</h1>
+  <p>{{ count }}</p>
+  <button v-on:click="incrementCount">Increment Count</button>
+  <h1>{{ incrementAmount }}</h1>
+  <p>{{ optimizedIncrementAmount }}</p>
+  <div>
+    <label for="incrementAmount">Increment by:</label>
+    <input type="number" v-model="incrementAmount" />
+  </div>
+  <hr />
+  <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
+  <p v-else>Odd: {{ message }}</p>
+  <ul>
+    <li v-for="(number, index) in listOfNumbers" :key="`number-${index}`">
+      {{ number }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
