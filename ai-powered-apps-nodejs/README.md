1ebb10fa23a1b10c713a768cc7be994256129e91
philipstubbs13 <philipstubbs13@gmail.com>
2023-12-02 05:56:16 -0600

    openai and nodejs

---
+++ ai-powered-apps-nodejs/README.md
@@ -0,0 +1,9 @@
+# Build AI-Powered Apps with OpenAI and Node.js
+
+* <https://frontendmasters.com/courses/openai-node/>
+* course notes: <https://scottmoss.notion.site/scottmoss/AI-App-Node-js-f9a372a138ef4241943b4fbb44bdc970>
+* course code: <https://github.com/frontendmasters/ai-nodejs>
+* <https://aibits.substack.com/p/paper-break-hyperdreambooth>
+* <https://cursor.sh/>
+* <https://marketplace.visualstudio.com/items?itemName=ktiays.aicursor>
+* openai usage dashboard: <https://platform.openai.com/usage>
---
+++ ai-powered-apps-nodejs/ai-course/chat.js
@@ -0,0 +1,49 @@
+import 'dotenv/config'
+import { openai } from './openai.js'
+import readline from 'node:readline'
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const newMessage = async (history, message) => {
+    const results = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [...history, message],
+        temperature: 0
+    })
+
+    return results.choices[0].message
+}
+
+const formatMessage = (userInput) => ({ role: 'user', content: userInput })
+const chat = () => {
+    const history = [
+        {
+            role: 'system',
+            content: 'You are an AI assistant. Answer questions or else!'
+        },
+    ]
+
+    const start = () => {
+        rl.question('You: ', async (userInput) => {
+            if (userInput.toLowerCase() === 'exit') {
+                rl.close()
+                return
+            }
+
+            const message = formatMessage(userInput)
+            const response = await newMessage(history, message)
+
+            history.push(message, response)
+            console.log(`\n\nAI: ${response.content}`)
+            start()
+        })
+    }
+
+    start()
+}
+
+console.log("Chatbot initialized. Type 'exit' to end the chat.")
+chat()
