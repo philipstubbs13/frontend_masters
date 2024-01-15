8899a3f56241ceff84d094979656ec024e0c96d4
philipstubbs13 <philipstubbs13@gmail.com>
2024-01-15 06:07:17 -0600

    vite

--- vite/vite-starter-main/src/index.js
+++ vite/vite-starter-main/src/index.js
@@ -1,6 +1,14 @@
-import { initializeCounter } from'./counter';
+import image from './steve-after-a-workshop.jpg'
+import { name } from './characters/gandorf.json';
 
-console.log('hello from index.js');
+console.log(name);
+
+const img = document.createElement('img');
+img.src = image;
+
+console.log(import.meta.env)
+
+document.querySelector('#content').appendChild(img);
 
 import('./counter').then(({ initializeCounter }) => {
     initializeCounter();
